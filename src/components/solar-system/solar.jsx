82d95c00ea8e3405e13fcc6b2076b253ebce3364
antico94 <meiuandrei@gmail.com>
2022-08-14 18:35:10 +0300

    first

---
+++ src/components/solar-system/solar.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import "./solar.css"
+
+const SolarSystem = () => {
+  return (
+      <div className='solar-system'>
+        <div className='sun'></div>
+        <div className='mercury'></div>
+        <div className='venus'></div>
+        <div className='earth'></div>
+        <div className='mars'></div>
+        <div className='jupiter'></div>
+        <div className='saturn'></div>
+        <div className='uranus'></div>
+        <div className='neptune'></div>
+        <div className='pluto'></div>
+        <div className='asteroids-belt'></div>
+      </div>
+
+  );
+};
+
+export default SolarSystem;
